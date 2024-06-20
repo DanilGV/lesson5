acb5765402c1d85f4767bc5f3b4af641b3fad54a
DanilGV <danil.gareev.2015@mail.ru>
2024-06-20 20:22:53 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+immutable_var = 1 , "World" , True , int
+print(immutable_var)
+#immutable_var [0] = 2. Кортеж содержит в себе неизменяемый тип данных, для сохранения первоначального значения
+mutable_list = ['World', 1 , True ]
+print(mutable_list)
+mutable_list [0] = 'sun'
+print(mutable_list)
+mutable_list.append(int)
+print(mutable_list)
+
+
+
